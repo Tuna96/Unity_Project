e71b2cbbf2dd5b33f7da446ccc9fbf0b59f818f5
Tuna96 <tu.hsgs@gmail.com>
2016-03-13 12:41:17 +0700

    Added files via upload

---
+++ Block_Breaker/Assets/Script/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	private bool hasStart;
+	private Vector3 paddleToBall = new Vector3();
+
+	public Paddle paddle;
+	// Use this for initialization
+	void Start () {
+		hasStart = false;
+		paddleToBall = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasStart) {
+			this.transform.position = paddleToBall + paddle.transform.position;
+			}
+		if (Input.GetMouseButtonDown (0)) {
+			hasStart = true;
+			this.GetComponent<Rigidbody2D>().velocity = new Vector2 (5f, 20f);
+		}			
+	}
+}
---
+++ Block_Breaker/Assets/Script/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    public void LevelLoad (string name) {
+        Debug.Log("New level loaded: " + name);
+        Application.LoadLevel (name);
+    }
+
+    public void QuitRequest ()
+    {
+        Debug.Log("Quit Requested");
+        Application.Quit();
+    }
+}
