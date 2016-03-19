1d36ac79bb03e485da340305f0f0a7c154d3edfd
Tuna96 <tu.hsgs@gmail.com>
2016-03-19 22:18:17 +0700

    Added files via upload

---
+++ Block_Breaker/Assets/Assets/Script/Ball.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	private bool hasStart;
+	private Vector3 paddleToBall = new Vector3();
+
+	private Paddle paddle;
+	// Use this for initialization
+	void Start () {
+		hasStart = false;
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBall = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasStart) {
+			this.transform.position = paddleToBall + paddle.transform.position;
+			}
+		if (Input.GetMouseButtonDown (0)) {
+			hasStart = true;
+			this.GetComponent<Rigidbody2D>().velocity = new Vector2 (5f, 20f);
+		}			
+	}
+}
